d68b6401ec5acd271ba579aff60cc640ed5534f0
Dominique Vilain <domi.vilain@gmail.com>
2016-09-21 11:22:02 +0200

    Ajout de la dépendance imagemin et commentaires généraux

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,25 @@
+// Définition des dépendances dont on a besoin pour exécuter les tâches
 var
-    gulp = require('gulp');
+    gulp = require('gulp'),
+    imagemin = require('gulp-imagemin');
 
+// Définition de quelques variables générales pour notre gulpfile
+var
+    source = 'source/',
+    dest = 'build/';
+
+// Définition de quelques variables liées à nos tâches (options de tâches)
+var
+    imagesOpts = {
+
+    };
+
+// Définition des tâches
+gulp.task('images', function(){
+    gulp.src()
+});
+
+// Tâche par défaut exécutée lorsqu’on tape juste *gulp* dans le terminal
 gulp.task('default', function(){
 
 });
