377b15ecb16c681fc1c32dee569d53fc7e65eb9c
Dominique Vilain <domi.vilain@gmail.com>
2016-09-21 12:03:01 +0200

    Added imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 // Définition des dépendances dont on a besoin pour exécuter les tâches
 var
     gulp = require('gulp'),
-    imagemin = require('gulp-imagemin');
+    imagemin = require('gulp-imagemin'),
+    newer = require('gulp-newer'),
+    size = require('gulp-size'),
+    del = require('del');
 
 // Définition de quelques variables générales pour notre gulpfile
 var
@@ -11,16 +14,28 @@ var
 // Définition de quelques variables liées à nos tâches (options de tâches)
 var
     imagesOpts = {
-
+        in: source + 'images/*.*',
+        out: dest + 'images/'
     };
 
+
 // Définition des tâches
-gulp.task('images', function(){
-    gulp.src()
+gulp.task('clean', function () {
+    del([dest + '*']);
+});
+
+gulp.task('images', function () {
+    gulp.src(imagesOpts.in)
+        .pipe(newer(imagesOpts.out))
+        .pipe(size({title: 'Images size before compression: ', showFiles: true}))
+        .pipe(imagemin())
+        .pipe(size({title: 'Images size after compression: ', showFiles: true}))
+        .pipe(gulp.dest(imagesOpts.out));
+
 });
 
 // Tâche par défaut exécutée lorsqu’on tape juste *gulp* dans le terminal
-gulp.task('default', function(){
+gulp.task('default', function () {
 
 });
 
